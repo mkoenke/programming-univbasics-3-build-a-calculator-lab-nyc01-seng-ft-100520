a28663b01c8f482eb49ea8cddc892030d9941f00
Mary Koenke <mkoenke@sas.upenn.edu>
2020-09-12 20:20:07 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -4,12 +4,12 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-  difference = num2 - num1
+  difference = num1 - num2
   difference
 end
 
 def division(num1, num2)
-  quotient = num2 / num1
+  quotient = num1 / num2
   quotient
 end
 
@@ -19,10 +19,11 @@ def multiplication(num1, num2)
 end
 
 def modulo(num1, num2)
-  remainder = num2 % num1
+  remainder = num1 % num2
   remainder
 end
 
 def square_root(num)
-  sqrt(num)
+  answer = sqrt(num)
+  answer
 end
